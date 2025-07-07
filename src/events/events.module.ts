c24de16988d692046731f6bffa7e682fb2b4884d
HungJuiChen <hungjui2@illinois.edu>
2025-07-07 20:08:19 +0800

    npm run start bug fix

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { EventsService } from './events.service';
 import { EventsController } from './events.controller';
+import { Event } from './event.entity';
+import { User } from '../users/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Event, User])],
   providers: [EventsService],
-  controllers: [EventsController]
+  controllers: [EventsController],
 })
 export class EventsModule {}
+
